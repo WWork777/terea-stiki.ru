6aa5c362fb3c2c9e86e319456556c24dd9b6126d
id0ntc4re <parsikovevgenij470@gmail.com>
2026-03-02 01:31:45 +0700

    qwe

--- components/ModalManager/BlockModal/BlockModal.jsx
+++ components/ModalManager/BlockModal/BlockModal.jsx
@@ -1,25 +1,25 @@
-'use client';
-import { useEffect, useState } from 'react';
-import styles from "./BlockModal.module.scss"
+"use client";
+import { useEffect, useState } from "react";
+import styles from "./BlockModal.module.scss";
 
 const BlockModal = ({ allowClose = false, onClose }) => {
   const [isVisible, setIsVisible] = useState(true);
-  const [currentDate, setCurrentDate] = useState('');
+  const [currentDate, setCurrentDate] = useState("");
 
   useEffect(() => {
     // Форматирование текущей даты
     const now = new Date();
-    const formattedDate = now.toLocaleDateString('ru-RU', {
-      day: 'numeric',
-      month: 'long',
-      year: 'numeric',
+    const formattedDate = now.toLocaleDateString("ru-RU", {
+      day: "numeric",
+      month: "long",
+      year: "numeric",
     });
     setCurrentDate(formattedDate);
 
     // Блокировка скролла
-    document.body.style.overflow = isVisible ? 'hidden' : 'auto';
+    document.body.style.overflow = isVisible ? "hidden" : "auto";
     return () => {
-      document.body.style.overflow = 'auto';
+      document.body.style.overflow = "auto";
     };
   }, [isVisible]);
 
@@ -41,7 +41,7 @@ const BlockModal = ({ allowClose = false, onClose }) => {
           <button
             className={styles.closeButton}
             onClick={handleClose} // Изменено с setIsVisible(false) на handleClose
-            aria-label='Закрыть уведомление'
+            aria-label="Закрыть уведомление"
           >
             &times;
           </button>
@@ -49,10 +49,13 @@ const BlockModal = ({ allowClose = false, onClose }) => {
         <h2>Добро пожаловать в Iluma Store</h2>
 
         <p>
-          <br/>
-Мы не осуществляем дистанционную розничную продажу никотиносодержащей продукции и устройств для потребления никотиносодержащей продукции.<br/>
-Информация на сайте не предназначена для несовершеннолетних. Подтвердите, что вам исполнилось 18 лет<br/>
-
+          <br />
+          Мы не осуществляем дистанционную розничную продажу никотиносодержащей
+          продукции и устройств для потребления никотиносодержащей продукции.
+          <br />
+          Информация на сайте не предназначена для несовершеннолетних.
+          Подтвердите, что вам исполнилось 18 лет
+          <br />
         </p>
 
         {/* <p>
@@ -61,22 +64,27 @@ const BlockModal = ({ allowClose = false, onClose }) => {
           дня!{' '}
         </p> */}
         <div className={styles.buttons}>
-            <button className={styles.continueButton}
-              onClick={handleClose} // Изменено с setIsVisible(false) на handleClose
-              aria-label='Закрыть уведомление'>
-                Да
-            </button>
-            <button className={styles.continueButton}
-              onClick={() => window.open("https://www.google.com/", "_self")}
-              aria-label='Закрыть уведомление'>
-                Нет
-            </button>
+          <button
+            className={styles.continueButton}
+            onClick={handleClose} // Изменено с setIsVisible(false) на handleClose
+            aria-label="Закрыть уведомление"
+          >
+            Да
+          </button>
+          <button
+            className={styles.continueButton}
+            onClick={() => window.open("https://www.google.com/", "_self")}
+            aria-label="Закрыть уведомление"
+          >
+            Нет
+          </button>
         </div>
         <p>
-          Сайт представляет собой интернет-витрину, позволяет ознакомиться с ассортиментом товаров и оформить бронирование для покупки в стационарном магазине. Информация на сайте не является рекламой и публичной офертой.
-
+          Сайт представляет собой интернет-витрину, позволяет ознакомиться с
+          ассортиментом товаров и оформить бронирование для покупки в
+          стационарном магазине. Информация на сайте не является рекламой и
+          публичной офертой.
         </p>
-        
       </div>
     </div>
   );
