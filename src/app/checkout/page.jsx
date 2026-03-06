b9ce2ce32507bd4267a577f757220b97772bba7c
Сергей Суровцев <sersur42@MacBook-Air-Sergej.local>
2026-03-06 08:40:12 +0700

    upd

--- src/app/checkout/page.jsx
+++ src/app/checkout/page.jsx
@@ -982,13 +982,14 @@ ${formattedCart}
       <div className="checkout-form">
         <div className="plitka">
           <h1>Оформление заказа</h1>
-          <h5>
-            ВАЖНО! Укажите Ваш номер в WhatsApp или Telegram ник для связи
-          </h5>
+          
         </div>
         <form onSubmit={handleSubmit} ref={formRef}>
           <div className="checkout-name">
             <h4>Контактные данные</h4>
+            <h5>
+              ВАЖНО! Укажите Ваш номер в WhatsApp или Telegram ник для связи
+            </h5>
             <input
               type="text"
               name="lastName"
